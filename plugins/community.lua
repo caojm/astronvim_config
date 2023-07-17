9ef1ee17d70645752d129251170256ece18a33ce
caojiaming <knowink@qq.com>
2023-07-17 15:56:21 +0800

    [ADD]nvim.surround

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,5 +8,6 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
   { import = "astrocommunity.motion.vim-matchup" },
+  { import = "astrocommunity.motion.nvim-surround" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
